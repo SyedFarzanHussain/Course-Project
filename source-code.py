f3e2195bfa8b3305f8c50a91f0816887b5fa8366
SyedFarzanHussain <farzan.dec@gmail.com>
2024-01-11 10:41:11 +0100

    Initial Data exploration was done to check the data

--- source-code.py
+++ source-code.py
@@ -11,9 +11,35 @@ unzipping dataset
 with zipfile.ZipFile("Course-Project\Dataset.zip", 'r') as zip_ref:
     zip_ref.extractall("Course-Project")
 '''
-
+#reading the dataset
 features_data=pd.read_csv("features.csv")
 stores_data=pd.read_csv("stores.csv")
 train_data=pd.read_csv("train.csv")
 
-print(features_data.head(10))
+#checking the value in the data
+
+features_data.head(10)
+stores_data.head(10)
+train_data.head(10)
+
+#Data Exploration
+
+features_data.describe().T
+features_data.info()
+train_data.describe().T
+train_data.info()
+stores_data.describe().T
+stores_data.info()
+
+#checkin Null Values
+
+features_data.isnull().sum()
+stores_data.isnull().sum()
+train_data.isnull().sum()
+
+
+
+
+
+
+
